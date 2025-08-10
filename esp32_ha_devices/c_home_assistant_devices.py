4737c10013d7acac0c8a0f1a48d69089fe9d6990
cuipu <g050505@gmail.com>
2025-08-10 21:45:14 +0800

    1

--- esp32_ha_devices/c_home_assistant_devices.py
+++ esp32_ha_devices/c_home_assistant_devices.py
@@ -20,7 +20,7 @@ OVER_WARINING_TEMPERATURE = 60
 
 DS18B20_GPIO_NUM = 19
 PASSIVE_BUZZER_GPIO_NUM = 23
-TEMPERATURE_SEND_MSG_FREQ = 2
+
 
 ESP32160LCD_SCL_GPIO_NUM = 22
 ESP32160LCD_SDA_GPIO_NUM = 21
@@ -32,10 +32,7 @@ HOMEASSISTANT_SWITCH_TYPE_RELAY_CONTROLLER = 'relay'
 
 MQTT_CLIENT_CHECK_MSG_FREQ = 0.1
 
-# 单路继电器
-RELAY_GPIO_NUM = 19
-RELAY_STATUS_GPIO_NUM = 18
-
+RELAY_GPIO_NUM = 22
 # 人体传感器
 INFRARED_MOTION_SENSOR_GPIO_NUM = 35
 
@@ -45,6 +42,11 @@ Light_Sensor_DIGITAL_GPIO_NUM = 21
 LIGHT_SENSOR_THRESHOLD = 4000
 
 RELAY_ON_TIME = 10
+
+# 温度传感器向HA发送数据的频率
+TEMPERATURE_SEND_MSG_FREQ = 300
+# LCD刷新频率
+LCD_REFRESH_FREQ = 1
 
 # MQTT_COMMAND_TOPIC = 'HA-esp32-relay/switch/set'
 # MQTT_STATE_TOPIC='HA-esp32-relay/switch/state'
@@ -58,8 +60,11 @@ class HATemperatureSensor(HomeAssistantSensorDevice):
 
         self.ds18b20_temperature_sensor = None
         self.passive_buzzer = None
+        self.time_util = TimeUtil()
         
-
+        self.last_send_time = 0  # 记录上次发送时间
+        self.last_refresh_time = 0  # 记录上次LCD刷新时间
+        self.last_temperature = None  # 缓存最新温度值
         self.init()
 
     def init_device_info(self, homeassistant_device_name: str = None, homeassistant_device_sensor_name: str = None,
@@ -94,13 +99,39 @@ class HATemperatureSensor(HomeAssistantSensorDevice):
 
     def do_work(self):
         while True:
+            current_time = self.time_util.get_current_timestamp() # 当前时间（秒）
+            print("current_time:", current_time)
+            
             device, temperature = self.ds18b20_temperature_sensor.collect_temperature_result()
-            self.do_mqtt_publish_device_msg(str(temperature))
-            self.esp32160lcd.show_msg(self.time_util.get_current_datetime_hms(), "T:" + str(temperature))
-            if temperature > OVER_WARINING_TEMPERATURE:
-                self.passive_buzzer.play_mario()
-            time.sleep(TEMPERATURE_SEND_MSG_FREQ)
-            self.esp32160lcd.clear_msg()
+            print("temperature:", temperature)
+            self.last_temperature = temperature  # 更新缓存温度
+            
+            
+            # 每5分钟发送一次MQTT数据
+            if current_time - self.last_send_time >= TEMPERATURE_SEND_MSG_FREQ:
+                self.do_mqtt_publish_device_msg(str(temperature))
+                self.last_send_time = current_time  # 更新发送时间
+            
+            # 每1秒刷新LCD
+            if current_time - self.last_refresh_time >= LCD_REFRESH_FREQ:
+                self.esp32160lcd.clear_msg()
+                # 显示时间和温度
+                self.esp32160lcd.show_msg(
+                    self.time_util.get_current_datetime_hms(),
+                    #current_time - self.last_send_time,
+                    "temp: " + str(self.last_temperature)
+                )
+                self.last_refresh_time = current_time  # 更新刷新时间
+                
+            #print("last_send_time:", self.last_send_time)
+            #print("last_refresh_time:", self.last_refresh_time)
+            #self.do_mqtt_publish_device_msg(str(temperature))
+            #self.esp32160lcd.show_msg(self.time_util.get_current_datetime_hms(),"hello")
+            #self.esp32160lcd.show_msg(self.time_util.get_current_datetime_hms(), "temp: " + str(temperature))
+            #if temperature > OVER_WARINING_TEMPERATURE:
+            #    self.passive_buzzer.play_mario()
+            time.sleep(0.1)
+            #self.esp32160lcd.clear_msg()
             
         '''  
         try:
@@ -144,7 +175,7 @@ class HASwitchDevice(HomeAssistantSwitchDevice):
 
         self.relay_gpio_num = RELAY_GPIO_NUM
 
-        self.relay = Relay(RELAY_GPIO_NUM,RELAY_STATUS_GPIO_NUM)
+        self.relay = Relay(RELAY_GPIO_NUM)
 
         # 人体传感器
         self.infrared_motion_sensor = InfraredMotionSensor(
@@ -189,8 +220,6 @@ class HASwitchDevice(HomeAssistantSwitchDevice):
     def do_work(self):
         while True:
                 self.mqtt_client.check_msg()
-                print("infrared_motion_sensor： " + str(self.infrared_motion_sensor.is_motion_detected())   + " light_sensor： " + str(self.light_sensor.read_light_analog()) 
-                + " get_relay_state： " + str(self.relay.get_relay_status()))
                 # 控制检测MQTT的频率
                 time.sleep(MQTT_CLIENT_CHECK_MSG_FREQ)
         '''
@@ -217,8 +246,8 @@ class HASwitchDevice(HomeAssistantSwitchDevice):
             # 重启设备
             machine.reset()
         '''
-    # def multi_thread_start_device(self):
-    #    self.multi_thread_util.start_new_thread(self.do_work)
+    def multi_thread_start_device(self):
+        self.multi_thread_util.start_new_thread(self.do_work)
 
     def start_device(self):
         self.do_work()
@@ -226,12 +255,12 @@ class HASwitchDevice(HomeAssistantSwitchDevice):
     def infrared_motion_sensor_hander(self, *arges):
         # 判断亮度是否需要开灯
         light_analog_value = self.light_sensor.read_light_analog()
-        if ((light_analog_value > LIGHT_SENSOR_THRESHOLD) and self.relay.get_relay_status() == 0):
+        if (light_analog_value > LIGHT_SENSOR_THRESHOLD):
             # 开灯
             self.relay.on()
             self.mqtt_client.publish(
                 self.homeassistant_switch_state_topic, "ON")
-            time.sleep(5)
+            time.sleep(10)
             self.relay.off()
             self.mqtt_client.publish(
                 self.homeassistant_switch_state_topic, "OFF")
